5608c6ed375b150894213553b0b566e90987988f
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-05-08 22:52:08 +0600

    style: format code for consistency and readability

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express =  require('express' );
+const express = require("express");
 const app = express();
-const cors = require('cors');
- const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
 
- app.use(cors());
- app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
+const uri = `mongodb+srv://simpleCrudUser:ib3eieJR8RJr8VMz@cluster0.sbrz6gk.mongodb.net/?appName=Cluster0`;
+app.get("/", (req, res) => {
+  res.send("Simple CRUD server is serving you!");
+});
 
-
- app.get('/', (req, res) => {
-    res.send('Simple CRUD server is serving you!');
-
- })
-
- app.listen(port, () => {
-    console.log(`Server is Running on Port: ${port}`);
- }) 
+app.listen(port, () => {
+  console.log(`Server is Running on Port: ${port}`);
+});

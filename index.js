16b8eeb6777b377f845c5f60701790b88157973f
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-05-09 14:57:06 +0600

    feat: add user retrieval by ID endpoint to fetch user details

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -31,6 +31,16 @@ const run = async () => {
          const cursor = usersCollection.find();
          const result = await cursor.toArray();
          res.send(result);
+
+      })
+
+
+      app.get('/users/:id', async (req, res) => { 
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const user = await usersCollection.findOne(query);
+        res.send(user)
+
 
       })
 
